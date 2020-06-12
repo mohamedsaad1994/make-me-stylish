ffd018cd28a837f7772c8db8ff3912c0a119a523
MOHAMED SAAD <mohamedsaad1994@outloo.com>
2020-06-12 16:46:55 +0200

    intial commit

---
+++ make-me-stylish.js
@@ -0,0 +1,10 @@
+function myFunc(elementsName) {
+    var objectStyle = { color: "red", background: "blue" };
+    var myElements = document.getElementsByTagName(elementsName);
+    for (var i = 0; i < myElements.length; i++) {
+        var e = myElements[i];
+        for (var j in objectStyle) {
+            e.style[j] = objectStyle[j];
+        }
+    }
+}
